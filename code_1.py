4067dfcaf2cfb707e5decb46e875ff84d59e09b6
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-11-24 00:44:08 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+
+        console.log("Random Number: " + item);
+    });
+    for (let i = 0; i < 10; i++) {
+    }
+function generateRandomData() {
+    return data;
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    data.forEach(item => {
+}
+
+function main() {
+}
+
+    const data = generateRandomData();
