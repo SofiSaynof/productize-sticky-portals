1447a0f5736b85f41953715c6693174ecbf5dc40
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-12-02 09:33:24 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,40 +1,16 @@
-    return data
-if __name__ == "__main__":
+    return data;
+function generateRandomData() {
 
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
+    const data = [];
 
-
-    for item in data:
-    main()
-def generate_random_data():
-def main():
-        print(f"Random Number: {item}")
-
-import random
-if __name__ == "__main__":
-
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-
-    main()
-def generate_random_data():
-import random
-    return data
-
-    for item in data:
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-import random
-    main()
-if __name__ == "__main__":
-def main():
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-    for item in data:
-    return data
-        print(f"Random Number: {item}")
+function main() {
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+    });
+    data.forEach(item => {
+}
+}
+    }
+main();
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
