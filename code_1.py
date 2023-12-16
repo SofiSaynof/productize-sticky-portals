e3daf419a321c800d03cb04048890c9da6cac5b5
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-12-16 08:34:12 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,43 +1,29 @@
-    data = generate_random_data()
 
-
-def main():
-    main()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    for item in data:
-    return data
-if __name__ == "__main__":
-import random
-        print(f"Random Number: {item}")
-
-    }
     for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
+    }
+    });
+    return data;
 
+
+}
+function main() {
+}
+function generateRandomData() {
 main();
         console.log("Random Number: " + item);
     const data = [];
-    });
-
-}
+    data.forEach(item => {
         data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
     const data = generateRandomData();
-function generateRandomData() {
-    return data;
-}    for item in data:
+        }
 
-def generate_random_data():
-    main()
-
-def main():
-if __name__ == "__main__":
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-
-        print(f"Random Number: {item}")
-
-    return data
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+    }
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+    public static void main(String[] args) {
+}
