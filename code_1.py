0c9564d57f04d90eab25fb8c56b63719a88e13d8
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-12-19 03:31:03 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,29 +1,13 @@
+if __name__ == "__main__":
+    main()
+    data = generate_random_data()
 
-    for (let i = 0; i < 10; i++) {
-    }
-    });
-    return data;
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+    return data
 
+import random
+    for item in data:
 
-}
-function main() {
-}
-function generateRandomData() {
-main();
-        console.log("Random Number: " + item);
-    const data = [];
-    data.forEach(item => {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    const data = generateRandomData();
-        }
-
-            data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
-    }
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-        }
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-    public static void main(String[] args) {
-}
